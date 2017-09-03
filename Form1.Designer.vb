6f35ff8463e8d992c911cc906ac0ae4973d21d79
TAKEDA Hiroyuki <leiqunni@gmail.com>
2017-09-04 06:55:08 +0900

    first commit.
    from https://monitoroff.codeplex.com/

---
+++ Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnTurnOff = New System.Windows.Forms.Button()
+        Me.btnOffAndLock = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnTurnOff
+        '
+        Me.btnTurnOff.Location = New System.Drawing.Point(6, 6)
+        Me.btnTurnOff.Name = "btnTurnOff"
+        Me.btnTurnOff.Size = New System.Drawing.Size(160, 60)
+        Me.btnTurnOff.TabIndex = 0
+        Me.btnTurnOff.Text = "Monitor Off"
+        Me.btnTurnOff.UseVisualStyleBackColor = True
+        '
+        'btnOffAndLock
+        '
+        Me.btnOffAndLock.Location = New System.Drawing.Point(6, 72)
+        Me.btnOffAndLock.Name = "btnOffAndLock"
+        Me.btnOffAndLock.Size = New System.Drawing.Size(160, 60)
+        Me.btnOffAndLock.TabIndex = 1
+        Me.btnOffAndLock.Text = "Turn Off and Lock"
+        Me.btnOffAndLock.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(172, 140)
+        Me.Controls.Add(Me.btnOffAndLock)
+        Me.Controls.Add(Me.btnTurnOff)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "MONITOROFF"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnTurnOff As Button
+    Friend WithEvents btnOffAndLock As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Private Shared Function PostMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function LockWorkStation() As Boolean
+    End Function
+
+    Private Const WM_SYSCOMMAND = &H112
+    Private Const SC_MONITORPOWER = &HF170
+
+    Private Const MONITOR_ON = -1
+    Private Const MONITOR_OFF = 2
+    Private Const MONITOR_STANDBY = 1
+
+    Private Sub btnTurnOff_Click(sender As Object, e As EventArgs) Handles btnTurnOff.Click
+        PostMessage(Me.Handle, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF)
+        End
+    End Sub
+
+    Private Sub btnOffAndLock_Click(sender As Object, e As EventArgs) Handles btnOffAndLock.Click
+        LockWorkStation()
+        PostMessage(Me.Handle, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF)
+        End
+    End Sub
+
+End Class
